c7fe05fdfb4887dc3ff84346f992db22e5c4a9cd
Arthur Creek <arthur.creek@gmail.com>
2014-10-22 16:02:04 +0400

    v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-gulp-md-gen
-===========
+gulp-md-gen v0.1.0
+=================
+
+Generate landing documentation from markdown files
+
--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var through = require('through');
 var path = require('path');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
-var File = gutil.File;
 var Buffer = require('buffer').Buffer;
-var Concat = require('concat-with-sourcemaps'); //!!!
 
 module.exports = function(file, opt) {
   if (!file) throw new PluginError('gulp-md-gen', 'Missing file option for gulp-md-gen');
@@ -12,53 +10,45 @@ module.exports = function(file, opt) {
   // to preserve existing |undefined| behaviour and to introduce |newLine: ""| for binaries
   if (typeof opt.newLine !== 'string') opt.newLine = gutil.linefeed;
 
-  var firstFile = null;
-
   var fileName = file;
   if (typeof file !== 'string') {
     if (typeof file.path !== 'string') {
       throw new PluginError('gulp-md-gen', 'Missing path in file options for gulp-md-gen');
     }
     fileName = path.basename(file.path);
-    firstFile = new File(file);
   }
 
-  var concat = new Concat(false, fileName, opt.newLine);
+  var title = opt.title ? opt.title :  "Documentation"
+  var content = "";
+  var content_list = title + "\n======\n";
+  var old_section = ''
 
   function bufferContents(file) {
     if (file.isNull()) return; // ignore
     if (file.isStream()) return this.emit('error', new PluginError('gulp-md-gen', 'Streaming not supported'));
 
-    console.log(file.relative);
+    var remove_ext = gutil.replaceExtension(file.relative, '');
+    var anchore = remove_ext.replace(/[\/\\\.]/g,'_').toLowerCase();
+    var title = file.contents.toString().replace(/^\s+/, '').split("\n")[0].replace(/^#+\s+/,'')
+    var section = path.dirname(file.relative)
 
+    content_list += "* [" + section + ": " + title + "](#" + anchore + ")\n";
 
-    if (!firstFile) firstFile = file;
-
-    concat.add(file.relative, file.contents.toString());
+    if(old_section != section)
+    {
+      old_section = section;
+      content += "\n"+section+"\n------";
+    }
+    content += "\n<a name=\"" + anchore + "\"></a>";
+    content += "\n" + file.contents.toString();
   }
 
   function endStream() {
-    if (firstFile) {
-      var joinedFile = firstFile;
-
-      if (typeof file === 'string') {
-        joinedFile = firstFile.clone({contents: false});
-        joinedFile.path = path.join(firstFile.base, file);
-      }
-
-      joinedFile.contents = new Buffer(concat.content);
-
-      if (concat.sourceMapping)
-        joinedFile.sourceMap = JSON.parse(concat.sourceMap);
-
-
-
-      this.emit('data', joinedFile);
-    }
-
-
-    console.log(this);
-
+    var file = new gutil.File({
+      path: fileName,
+      contents: new Buffer(content_list + content)
+    });
+    this.emit('data', file);
     this.emit('end');
   }
 
